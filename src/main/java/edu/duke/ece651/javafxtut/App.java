17782acf61afdfe74c98d988064949d6c9fd464a
Xueyang Liu <xueyang.liu@qq.com>
2021-03-26 00:56:23 +0800

    add css and xml

--- src/main/java/edu/duke/ece651/javafxtut/App.java
+++ src/main/java/edu/duke/ece651/javafxtut/App.java
@@ -4,59 +4,22 @@
 package edu.duke.ece651.javafxtut;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class App extends Application {
   @Override
-  public void start(Stage stage) {
-//    String javaVersion = System.getProperty("java.version");
-//    String javafxVersion = System.getProperty("javafx.version");
-//    Label l = new Label("Hello, JavaFX " + javafxVersion +
-//                        ", running on Java " +javaVersion + ".");
-//    Scene scene = new Scene(new StackPane(l), 640, 480);
-    GridPane gp = new GridPane();
-    String[] labels= new String[] {"+", "-", "*", "/",
-            "7", "8", "9",
-            "4", "5", "6",
-            "1", "2","3",
-            "."};
-    int[] rows = {0,0,0,0,1,1,1,2,2,2,3,3,3,4};
-    int[] cols = {0,1,2,3,0,1,2,0,1,2,0,1,2,2};
-    for (int i =0; i < labels.length; i++) {
-      Button b = new Button(labels[i]);
-      gp.add(b, cols[i], rows[i]);
-      b.setMaxWidth(Double.MAX_VALUE);
-      b.setMaxHeight(Double.MAX_VALUE);
-    }
-    Button b = new Button("0");
-    b.setMaxWidth(Double.MAX_VALUE);
-    b.setMaxHeight(Double.MAX_VALUE);
-    gp.add(b, 0,4,2,1);
-
-    b = new Button("E\nn\nt\ne\nr");
-    b.setMaxWidth(Double.MAX_VALUE);
-    b.setMaxHeight(Double.MAX_VALUE);
-    gp.add(b, 3,1,1,3);
-
-    for (int i =0; i < 4; i++) {
-      ColumnConstraints cc = new ColumnConstraints();
-      cc.setPercentWidth(25);
-      gp.getColumnConstraints().add(cc);
-    }
-    for(int i =0; i < 5; i++) {
-      RowConstraints rc = new RowConstraints();
-      rc.setPercentHeight(20);
-      gp.getRowConstraints().add(rc);
-    }
-
+  public void start(Stage stage) throws IOException {
+    URL xmlResource = getClass().getResource("/ui/calcbuttons.xml");
+    GridPane gp = FXMLLoader.load(xmlResource);
     Scene scene = new Scene(gp,640,480);
+    URL cssResource = getClass().getResource("/ui/calcbuttons.css");
+    scene.getStylesheets().add(cssResource.toString());
     stage.setScene(scene);
     stage.show();
   }
